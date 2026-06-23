1e7644b3a5184c8fc1fe6d9480dec1bbdcb02745
silc-o <bhussienaldondie@gmail.com>
2026-06-23 19:30:02 +0800

    feat: added an operate functions where it will do an operation based on the operator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+// basic calculator functions
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
 }
@@ -14,7 +15,27 @@ function divide(firstNum, secondNum) {
   return firstNum / secondNum;
 }
 
+let firstNum = null;
+let operator = null;
+let secondNum = null;
+
+function operate(firstNum, operator, secondNum) {
+  if (operator === "*") {
+    return multiply(firstNum, secondNum);
+  } else if (operator === "/") {
+    return divide(firstNum, secondNum);
+  } else if (operator === "+") {
+    return add(firstNum, secondNum);
+  } else if (operator === "-") {
+    return subtract(firstNum, secondNum);
+  }
+}
+
 console.log(add(3, 4));
 console.log(subtract(3, 4));
 console.log(multiply(3, 4));
 console.log(divide(3, 4));
+console.log(operate(10, "+", 5))
+console.log(operate(10, "-", 5))
+console.log(operate(10, "*", 5))
+console.log(operate(10, "/", 5))
