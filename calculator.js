ee575a3a93adf939e67cfd0c6180f8a85c291376
silc-o <bhussienaldondie@gmail.com>
2026-06-23 19:14:03 +0800

    feat: add initial HTML and basic calculator functions

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+  return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+  return firstNum / secondNum;
+}
+
+console.log(add(3, 4));
+console.log(subtract(3, 4));
+console.log(multiply(3, 4));
+console.log(divide(3, 4));
