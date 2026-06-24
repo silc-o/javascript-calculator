46e79dc411c5a1a3214753b72656cedac9ea9650
silc-o <bhussienaldondie@gmail.com>
2026-06-24 21:00:31 +0800

    feat: Add operator and number button handling to calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+// TODO: fix when clicking number instead of operator when the first equate is done. It does not reset the calculator nor does the firstEquation becomes the new firstNumValue
+
+const buttonsContainer = document.querySelector("#calculator-buttons");
+
 // basic calculator functions
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
@@ -19,23 +23,74 @@ let firstNum = null;
 let operator = null;
 let secondNum = null;
 
-function operate(firstNum, operator, secondNum) {
+let operatorChoices = {
+  addition: "+",
+  subtraction: "-",
+  multiplication: "*",
+  division: "/"
+}
+
+let operatorChoicesArray = Object.values(operatorChoices);
+
+function operate(firstNumValue, operatorValue, secondNumValue) {
+  firstNumValue = Number(firstNumValue)
+  secondNumValue = Number(secondNumValue)
   if (operator === "*") {
-    return multiply(firstNum, secondNum);
+    return multiply(firstNumValue, secondNumValue);
   } else if (operator === "/") {
-    return divide(firstNum, secondNum);
+    return divide(firstNumValue, secondNumValue);
   } else if (operator === "+") {
-    return add(firstNum, secondNum);
+    return add(firstNumValue, secondNumValue);
   } else if (operator === "-") {
-    return subtract(firstNum, secondNum);
+    return subtract(firstNumValue, secondNumValue);
   }
 }
 
-console.log(add(3, 4));
-console.log(subtract(3, 4));
-console.log(multiply(3, 4));
-console.log(divide(3, 4));
-console.log(operate(10, "+", 5))
-console.log(operate(10, "-", 5))
-console.log(operate(10, "*", 5))
-console.log(operate(10, "/", 5))
+buttonsContainer.addEventListener('click', (event) => {
+  // ignore clicks that aren't on an actual button
+  if (event.target.tagName !== "BUTTON") {
+    return; 
+  }
+
+  const clickedValue = event.target.textContent.trim();
+  clickButton(clickedValue);
+})
+
+function clickButton(value) {
+  // if = is clicked, equate
+  if (value === "=") {
+    console.log(operate(firstNum, operator, secondNum))
+  } else if (value === operatorChoices && firstNum === null) { // if empty, cant choose operator first
+    alert("Enter a number first");
+  } else if (operatorChoicesArray.includes(value) && operator === null) { // gets the operator
+    operator = value;
+    console.log(operatorChoices);
+  } else {
+    // gets the first pair of numbers
+    if (firstNum === null) {
+      firstNum = value;
+      console.log(firstNum);
+    } else if (firstNum != null && !operatorChoicesArray.includes(value) && operator === null) {
+      firstNum += value;
+      console.log(firstNum);
+    } 
+
+    // gets the second pair
+    if (firstNum != null && operator != null && !operatorChoicesArray.includes(value) && secondNum === null) {
+      secondNum = value;
+      console.log(secondNum);
+    } else if (secondNum != null && !operatorChoicesArray.includes(value)) {
+      secondNum += value;
+      console.log(secondNum);
+    } 
+
+    // if a second operator is called, calculate the first two pair of numbers first 
+    if (operatorChoicesArray.includes(value) && secondNum != null) {
+      let newFirstNum = operate(firstNum, operator, secondNum);
+      firstNum = newFirstNum;
+      operator = value;
+      console.log(firstNum);
+    }
+  }
+
+}
