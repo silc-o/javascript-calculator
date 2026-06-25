95bb6f0a2e56f0cdf793c7e70c3c534da8e3a51c
silc-o <bhussienaldondie@gmail.com>
2026-06-25 21:37:18 +0800

    debug: added error check for divide by 0, fixed when clicking operator or = when there is no value

--- calculator.js
+++ calculator.js
@@ -3,7 +3,6 @@
 const buttonsContainer = document.querySelector("#calculator-buttons");
 const calculatorDisplay = document.querySelector("#calculator-display");
 
-
 // basic calculator functions
 function add(firstNum, secondNum) {
   return firstNum + secondNum;
@@ -20,44 +19,6 @@ function multiply(firstNum, secondNum) {
 function divide(firstNum, secondNum) {
   return firstNum / secondNum;
 }
-
-let firstNum = null;
-let operator = null;
-let secondNum = null;
-let finalResult = null;
-
-let operatorChoices = {
-  addition: "+",
-  subtraction: "-",
-  multiplication: "*",
-  division: "/"
-}
-
-let operatorChoicesArray = Object.values(operatorChoices);
-
-function operate(firstNumValue, operatorValue, secondNumValue) {
-  firstNumValue = Number(firstNumValue)
-  secondNumValue = Number(secondNumValue)
-  if (operator === "*") {
-    return multiply(firstNumValue, secondNumValue);
-  } else if (operator === "/") {
-    return divide(firstNumValue, secondNumValue);
-  } else if (operator === "+") {
-    return add(firstNumValue, secondNumValue);
-  } else if (operator === "-") {
-    return subtract(firstNumValue, secondNumValue);
-  }
-}
-
-buttonsContainer.addEventListener('click', (event) => {
-  // ignore clicks that aren't on an actual button
-  if (event.target.tagName !== "BUTTON") {
-    return; 
-  }
-
-  const clickedValue = event.target.textContent.trim();
-  clickButton(clickedValue);
-})
 
 function displayValue(value) {
   const div = document.createElement("div");
@@ -79,6 +40,52 @@ function resetParameters() {
   operator = null;
   secondNum = null;
 }
+
+function operate(firstNumValue, operatorValue, secondNumValue) {
+  firstNumValue = Number(firstNumValue)
+  secondNumValue = Number(secondNumValue)
+  if (operator === "*") {
+    return multiply(firstNumValue, secondNumValue);
+  } else if (operator === "/") {
+    if (secondNumValue === 0) {
+      clearDisplay();
+      resetParameters();
+      alert("dividing by 0 is not possible");
+      return null;
+    } else {
+      return divide(firstNumValue, secondNumValue);
+    }
+  } else if (operator === "+") {
+    return add(firstNumValue, secondNumValue);
+  } else if (operator === "-") {
+    return subtract(firstNumValue, secondNumValue);
+  }
+}
+
+let firstNum = null;
+let operator = null;
+let secondNum = null;
+let finalResult = null;
+
+let operatorChoices = {
+  addition: "+",
+  subtraction: "-",
+  multiplication: "*",
+  division: "/"
+}
+
+let operatorChoicesArray = Object.values(operatorChoices);
+
+buttonsContainer.addEventListener('click', (event) => {
+  // ignore clicks that aren't on an actual button
+  if (event.target.tagName !== "BUTTON") {
+    return; 
+  }
+
+  const clickedValue = event.target.textContent.trim();
+  clickButton(clickedValue);
+})
+
 function clickButton(value) {
 
   if (finalResult != null) {
@@ -88,34 +95,53 @@ function clickButton(value) {
 
   // if = is clicked, equate
   if (value === "=") {
+    if (firstNum === null) {
+      return;
+    }
     finalResult = String(operate(firstNum, operator, secondNum));
-    clearDisplay();
-    calculatorDisplay.appendChild(displayValue(finalResult));
-    console.log(finalResult)
-  } else if (value === operatorChoices && firstNum === null) { // if empty, cant choose operator first
-    alert("Enter a number first");
-  } else if (operatorChoicesArray.includes(value) && operator === null) { // gets the operator
+    if (finalResult !== "null") {
+      finalResult = String(finalResult);
+      clearDisplay();
+      calculatorDisplay.appendChild(displayValue(finalResult));
+    }
+  }
+  
+  else if (operatorChoicesArray.includes(value) && firstNum != null && operator === null) { // gets the operator
     calculatorDisplay.appendChild(displayValue(value));
     operator = value;
     console.log(operator);
-  } else if (value === "C" || value === "AC") {
+  }
+
+  else if (calculatorDisplay.lastElementChild && operatorChoicesArray.includes(calculatorDisplay.lastElementChild.textContent.trim()) && operatorChoicesArray.includes(value) && secondNum === null) { // checks if the value clicked is an operator when there is an existing operator
+    const lastValueDiv = calculatorDisplay.lastElementChild;
+    lastValueDiv.remove();
+    calculatorDisplay.appendChild(displayValue(value));
+    operator = value;
+  }
+  
+  else if (value === "C" || value === "AC") {
     if (value === "AC") {
       clearDisplay();
       resetParameters();
     } else {
       const lastValueDiv = calculatorDisplay.lastElementChild;
       const lastValueContent = lastValueDiv.textContent.trim();
-      if (operatorChoicesArray.includes(lastValueContent)) {
+
+      if (operatorChoicesArray.includes(lastValueContent)) { // removes operator
         lastValueDiv.remove();
         operator = null;
-      } else if (operator === null) { // removes firstNum
+      } 
+      
+      else if (operator === null) { // removes firstNum
         if (firstNum.length === 1) {
           lastValueDiv.remove();
           firstNum = null;
         } else {
           lastValueDiv.remove();
         }
-      } else if (operator != null) {
+      }
+      
+      else if (operator != null) { // removes secondNum
         if (secondNum.length === 1) {
           lastValueDiv.remove();
           secondNum = null;
@@ -124,7 +150,12 @@ function clickButton(value) {
         }
       }
     }
-  } else {
+  } 
+  
+  else {
+    if (operatorChoicesArray.includes(value) && firstNum === null ) {
+      return; // exit clickButton entirely, do nothing
+    }
     calculatorDisplay.appendChild(displayValue(value));
     // gets the first pair of numbers
     if (firstNum === null) {
